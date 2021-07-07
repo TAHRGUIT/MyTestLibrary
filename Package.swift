33d36966383ed21601c52c43dd79cbb78fa688b6
yassin01 <yassine@ajicreative.club>
2021-07-07 11:55:07 +0200

    test Packaging

--- Package.swift
+++ Package.swift
@@ -5,10 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "MyTestLibrary",
+    platforms: [
+      .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MyTestLibrary",
+            type: .dynamic,	
             targets: ["MyTestLibrary"]),
     ],
     dependencies: [
